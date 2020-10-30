2b8c1bb72209d272fa2e645c92d428314fb20780
mahmoud hassan <heshamalamam73@gmail.com>
2020-10-30 13:29:21 +0100

    22

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ import data from "./data";
 import config from "./config";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-const mongodbUrl = process.env.MONGODB_URL;
+const mongodbUrl = config.MONGODB_URL;
 import userRoute from "./routes/userRoute.js";
 import producRoute from "./routes/productRoute";
 import bodyParser from "body-parser";
-
 dotenv.config();
+require("dotenv").config({ path: "MONGODB_URL" });
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("api works good");
+});
 mongoose
   .connect(mongodbUrl, {
     useNewUrlParser: true,
